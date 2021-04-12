7dcf75216f75bbd6d67bac3b282320576784b2ec
Aydan Pirani <aydanpirani@gmail.com>
2021-04-12 00:59:37 -0700

    Added add function

--- Trie.py
+++ Trie.py
@@ -1,10 +1,23 @@
 class TrieNode:
     def __init__(self, char=None):
-        self.value = char if char else "HEAD"
-        self.children = {}
+        self.value = char if char else "HEAD" #Value
+        self.children = {} #Children/child-nodes
+        self.has_entry = False #Whether or not this marks the end of a word
 
+    def add(self, string):
+        current = self
+        # Iterate through every character, and add it if it exists
+        for s in string:
+            current = current.children
+            if s not in current:
+                current[s] = TrieNode(s)
+            current = current[s]
+        # Clarifies that a word ends here (for the autocomplete/remove feature)
+        current.has_entry = True
 
 t = TrieNode()
+t.add("abcd")
+t.add("giraffe")
 v = t.value
 c = t.children
 print(t)
