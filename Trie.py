6b45c808ba010bfc6f94548d838455cbe3a8fe63
Aydan Pirani <aydanpirani@gmail.com>
2021-04-12 13:23:09 -0700

    Added delete method

--- Trie.py
+++ Trie.py
@@ -15,19 +15,59 @@ class TrieNode:
         # Clarifies that a word ends here (for the autocomplete/remove feature)
         current.has_entry = True
 
+    # Points to delete from: intersection, last word 
+    def delete(self, string):
+        # Test if there is no string to delete
+        if string == "":
+            return
+
+        current = self
+        del_key, del_loc = string[0], self.children
+
+        for i in range(len(string)):
+            s = string[i]
+
+            current = current.children
+            # Check if the string exists
+            if s not in current:
+                return    
+
+            # Gets the deletion point: last possible intersection or last possible non-current overlapping word
+            if len(current[s].children) > 1 or (current[s].has_entry and s != string[-1]):
+                del_loc = current[s].children
+                del_key = string[i+1]
+
+            current = current[s]
+
+        # If more children exist, then change to false, else delete from del_loc
+        if len(current.children) > 0:
+            del_loc[del_key].has_entry = False
+        else:
+            del_loc.pop(del_key, None)        
+
     # Stack-based implementation to print everything in trie
     def display(self, depth = 0):
-        print("-"*depth + self.value + (" (T)" if self.has_entry else ""))
+        if self.value == "HEAD":
+            depth -= 1
+        else:
+            print("-"*depth + self.value + (" (T)" if self.has_entry else ""))
         # Recursively displays values for every node in the trie
         for char, node in self.children.items():
             node.display(depth+1)
+        
+        if depth == -1:
+            print("\n")
+
 
 t = TrieNode()
 t.add("abcd")
 t.add("giraffe")
-v = t.value
-c = t.children
-print(t)
-print(v)
-print(c)
+t.add("git")
+t.add("github")
+
 t.display()
+# t.delete("git")
+# t.delete("github")
+# t.delete("giraffe")
+# t.delete("abcd")
+# t.display()
