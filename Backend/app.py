353b0855a7f5ea389c66a6e1589fd2782c1200f3
Aydan Pirani <aydanpirani@gmail.com>
2021-04-16 01:38:30 -0700

    Fixed backend server to handle multiple reqs (locks)

--- Backend/app.py
+++ Backend/app.py
@@ -1,48 +1,54 @@
 from flask import Flask, request
 from trie import *
+import threading
 
 app = Flask(__name__)
 trie = TrieNode()
+lock = threading.Lock()
 
 @app.route('/')
 def hello():
     data = request.json
-    print(data)
-    return "Text!"
+    return "Server is running!"
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
-    string = request.form["string"]
-    trie.add(string)
-    return f"The key \"{string}\" has been added to the trie!\n"
+    with lock:
+        string = request.form["string"]
+        trie.add(string)
+        return f"The key \"{string}\" has been added to the trie!\n"
 
 @app.route("/delete", methods=["GET", "POST"])
 def delete():
-    string = request.form["string"]
-    trie.delete(string)
-    return f"The key \"{string}\" has been removed from the trie!\n"
+    with lock:
+        string = request.form["string"]
+        trie.delete(string)
+        return f"The key \"{string}\" has been removed from the trie!\n"
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
-    string = request.form["string"]
-    in_trie = trie.search(string)
-    return f"The key \"{string}\" was " +  ("" if in_trie else "not ") + "found in the existing trie!\n"
+    with lock:
+        string = request.form["string"]
+        in_trie = trie.search(string)
+        return f"The key \"{string}\" was " +  ("" if in_trie else "not ") + "found in the existing trie!\n"
 
 @app.route("/autocomplete", methods=["GET", "POST"])
 def autocomplete():
-    prefix = request.form["string"]
-    words = trie.autocomplete(prefix)
-    if len(words) < 1:
-        return f"No autocomplete results for the prefix \"{prefix}\" were found in the trie!\n"
-    else:
-        return f"The prefix \"{prefix}\" has the following autocomplete results:\n{words}\n"
+    with lock:
+        prefix = request.form["string"]
+        words = trie.autocomplete(prefix)
+        if len(words) < 1:
+            return f"No autocomplete results for the prefix \"{prefix}\" were found in the trie!\n"
+        else:
+            return f"The prefix \"{prefix}\" has the following autocomplete results:\n{words}\n"
 
 @app.route("/display", methods=["GET", "POST"])
 def display():
-    stringified_trie = trie.display()
-    if len(stringified_trie.split()) == 0:
-        stringified_trie = "\nNULL (Empty)\n"
-    return f"The current state of the trie is as such:{stringified_trie}"
+    with lock:
+        stringified_trie = trie.display()
+        if len(stringified_trie.split()) == 0:
+            stringified_trie = "\nNULL (Empty)\n"
+        return f"The current state of the trie is as such:{stringified_trie}"
 
 @app.route("/clear", methods=["GET", "POST"])
 def clear():
